5873e51da78a9ec215571caf6e80c3bb236a8b1d
Anshuman151 <91521260+Anshuman151@users.noreply.github.com>
2022-07-04 21:52:06 +0530

    Delete Codechef directory

--- Codechef/Adding two integers.py
+++
@@ -1,5 +0,0 @@
-(a,b) = map(int,input().split())
-ans=a+b
-print(ans)
-c=list(map(int,input().split()))
-print(c)
--- Codechef/Practice make us Perfect.py
+++
@@ -1,8 +0,0 @@
-(p1,p2,p3,p4)=map(int,input().split())
-sum=0
-for i in (p1,p2,p3,p4):
-    if i >= 10 :
-        sum+=1
-    elif i < 10:
-        sum+=0
-print(sum)
